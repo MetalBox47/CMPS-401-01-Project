7cb98003081cba5991314552627dab7a5fa4fe35
Marco <mfigueroaferez@gmail.com>
2022-09-23 14:48:13 -0500

    adding test assets for a sample card, adjusted margins

--- Cards/CardBase.gd
+++ Cards/CardBase.gd
@@ -1,14 +1,24 @@
 extends MarginContainer
 
-
-# Declare member variables here. Examples:
+# Database variables are preloaded
 onready var CardDatabase = preload("res://Cards/CardsDatabase.gd")
-var CardName = "Counter"
+onready var creatures = CardDatabase.CREATURES
+onready var spells = CardDatabase.SPELLS
 
-# Called when the node enters the scene tree for the first time.
+onready var CardImg = str("res://Assets/Umbot_Factory.png")
+var Card
 func _ready():
-	print(CardDatabase.DATA["Creatures"]["Umbot_Skal"].name)
-
+	
+	var CardSize = rect_size  # rect_size is the size of the entire container (CardBase)
+	
+	# loading the CardImg and setting its scale to the card borders
+	Card = get_node("CardImgContainer/Card")
+	Card.texture = load(CardImg)
+	Card.scale *= $CardImgContainer.get_size()/Card.texture.get_size()
+	Card.position = $CardImgContainer.get_position()
+	
+	print($Background.scale)
+	print(Card.texture.get_size())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
