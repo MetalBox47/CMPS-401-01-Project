4cd4644142a1286a4e070c7c061696a0ebb4bf5d
Marquinoli <mfigueroaferez@gmail.com>
2022-10-09 21:47:15 -0500

    Built a dynamic card template
    depending on the "CardType" and "CardName", the script displays a certain card

--- Cards/CardBase.gd
+++ Cards/CardBase.gd
@@ -6,21 +6,51 @@ onready var creatures = CardDatabase.DATA.Creatures
 onready var spells = CardDatabase.DATA.Spells
 onready var creatures_arr = CardDatabase.CREATURES
 
-onready var CardImg = str("res://Assets/",spells["Umbot_Factory"].name, ".png")
+# We define the variables that are gonna be necessary to extract the assets
+var CardType = 'Spells'
+var CardName = 'Umbot_Factory'
 
-var Card
+# Here we filter which Card Background and which Card Image to use
+# Card Background = Frame (Depends on rarity)
+# Card Image = The small img inside the Frame (depends on card name)
+onready var CardInfo = CardDatabase.DATA[CardType][CardName]
+onready var CardImg = str("res://Assets/",CardInfo.name, ".png")
+onready var CardBackgroundImg = str("res://Asset/",CardInfo.rarity,".png")
+
+var CardTexture
 var CardImgContainer
+
 func _ready():
-	var CardSize = rect_size  # rect_size is the size of the entire container (CardBase)
 	
-	# loading the CardImg and setting its scale to the card borders
+	var CardSize = rect_size  # rect_size is the size of the "CardBase" container
+	# We may not use CardSize, this is just to show how to extract it.
+	
+	# loading the small CardImg and setting its scale to the card borders
 	CardImgContainer = get_node("HBoxContainer/Elements/CardImgContainer")
-	Card = get_node("HBoxContainer/Elements/CardImgContainer/Card")
-	Card.texture = load(CardImg)
-	Card.scale *= CardImgContainer.get_size()/Card.texture.get_size()
+	CardTexture = get_node("HBoxContainer/Elements/CardImgContainer/Card")
+	$HBoxContainer/Elements/CardImgContainer/Card.texture = load(CardImg)
+	$HBoxContainer/Elements/CardImgContainer/Card.scale *= CardImgContainer.get_size()/CardTexture.texture.get_size()
+	
+	# Loading the background texture
+	$Background.texture = load(CardBackgroundImg)
+	match CardType:
+		"Spells":
+			$HBoxContainer/Elements/TopBar/Title/Title.text = CardInfo["name"]
+			$HBoxContainer/Elements/Effect/Label.text = CardInfo["effect"]
+		"Creatures":
+			var Attack = CardInfo["top_counter"]
+			var Health = CardInfo["bot_counter"]
+			$HBoxContainer/Elements/TopBar/Title/Title.text = CardInfo["name"]
+			$HBoxContainer/Elements/Effect/Label.text = CardInfo["effect"]
+			$HBoxContainer/Elements/HBoxContainer/Health/Label.text = str(Health)
+			$HBoxContainer/Elements/HBoxContainer/Attack/Label.text = str(Attack)
+			
+	
+		
+	
 	
 	
 	print($Background.scale)
-	print(Card.texture.get_size())
+	print(CardTexture.texture.get_size())
 
 
--- Cards/CardsDatabase.gd
+++ Cards/CardsDatabase.gd
@@ -11,6 +11,7 @@ enum SPELLS {Counter, Umbot_Parade, Umbot_Factory, Clear_Weather}
 const DATA = {
 	"Creatures" : {
 		"Umbot_Lanu" : {
+			"rarity" : "CommonCard",
 			"name" : "Umbot Lanu",
 			"type" : "Robotic",
 			"top_counter" : "1",
@@ -19,6 +20,7 @@ const DATA = {
 			"effect" : "When this creature is summoned, draw 1 card. During an eclipse, add +1/+0 counter for every “Umbot” creature on your side of the field.",
 		},
 		"Umbot_Zuma" : {
+			"rarity" : "CommonCard",
 			"name" : "Umbot Zuma",
 			"type" : "Robotic",
 			"top_counter" : "1",
@@ -27,6 +29,7 @@ const DATA = {
 			"effect" : "When this creature is summoned, gain 1 energy. During an eclipse, add a +1/+0 counter for every “Umbot” creature on your side of the field.",
 		},
 		"Umbot_Plum" : {
+			"rarity" : "CommonCard",
 			"name" : "Umbot Plum",
 			"type" : "Robotic",
 			"top_counter" : "1",
@@ -34,6 +37,7 @@ const DATA = {
 			"cost" : 3,
 			"effect" : "When this creature is summoned, gain 1 life. During an eclipse, add a +1/+0 counter for every “Umbot” creature on your side of the field.",
 		},"Umbot_Fisa" : {
+			"rarity" : "CommonCard",
 			"name" : "Umbot Fisa",
 			"type" : "Robotic",
 			"top_counter" : "1",
@@ -41,6 +45,7 @@ const DATA = {
 			"cost" : 3,
 			"effect" : "When this creature is summoned, send the top card of your opponent’s deck to the graveyard. During an eclipse, add a +1/+0 counter for every “Umbot” creature on your side of the field.",
 		},"Umbot_Skal" : {
+			"rarity" : "CommonCard",
 			"name" : "Umbot Skal",
 			"type" : "Robotic",
 			"top_counter" : "1",
@@ -48,6 +53,7 @@ const DATA = {
 			"cost" : 3,
 			"effect" : "When this creature is summoned, your opponent takes 1 damage. During an eclipse, add a +1/+0 counter for every “Umbot” creature on your side of the field.",
 		},"Umbot_Champion_Vila" : {
+			"rarity" : "RareCard",
 			"name" : "Umbot Champion Vila",
 			"type" : "Robotic",
 			"top_counter" : "4",
@@ -55,6 +61,7 @@ const DATA = {
 			"cost" : 12,
 			"effect" : "Once per turn, you can discard 1 card to destroy 1 creature your opponent controls. During an eclipse, add a +2/+3 counter to this card.",
 		},"Lunarbird_Eule" : {
+			"rarity" : "LegendaryCard",
 			"name" : "Lunarbird Eule",
 			"type" : "Dark",
 			"top_counter" : "2",
@@ -62,6 +69,7 @@ const DATA = {
 			"cost" : 6,
 			"effect" : "When the environment is an eclipse, add a +0/+2 counter to this card. When this card is summoned, set the environment to eclipse.",
 		},"Weatherbird_Sonnenfalke" : {
+			"rarity" : "RareCard",
 			"name" : "Weatherbird Sonnenfalke",
 			"type" : "Fire",
 			"top_counter" : "2",
@@ -70,6 +78,7 @@ const DATA = {
 			"effect" : "When the environment is sunny, add a +0/+2 counter to this card. When this card is summoned, set the environment to sunny.",
 		},
 		"Weatherbird_Regengans" : {
+			"rarity" : "RareCard",
 			"name" : "Weatherbird Regengans",
 			"type" : "Water",
 			"top_counter" : "2",
@@ -78,6 +87,7 @@ const DATA = {
 			"effect" : "When the environment is rainy, add a +0/+2 counter to this card. When this card is summoned, set the environment to rainy.",
 		},
 		"Weatherbird_Albatros" : {
+			"rarity" : "RareCard",
 			"name" : "Weatherbird Albatros",
 			"type" : "Wind",
 			"top_counter" : "2",
@@ -86,35 +96,132 @@ const DATA = {
 			"effect" : "When the environment is windy, add a +0/+2 counter to this card. When this card is summoned, set the environment to windy.",
 		},
 		"Weatherbird_Geier" : {
+			"rarity" : "RareCard",
 			"name" : "Weatherbird Geier",
 			"type" : "Earth",
 			"top_counter" : "2",
 			"bot_counter" : "2",
 			"cost" : 6,
 			"effect" : "When the environment is dusty, add a +0/+2 counter to this card. When this card is summoned, set the environment to dusty.",
+		},
+		"Chlorchestra_Blossoon" : {
+			"rarity" : "CommonCard",
+			"name" : "Chlorchestra Blossoon",
+			"type" : "Earth",
+			"top_counter" : "2",
+			"bot_counter" : "3",
+			"cost" : 6,
+			"effect" : "Once per turn, if the environment is sunny, you can summon one Plant creature from your hand without paying its cost, but that creature becomes a 0/1.",
 		}, 
+		"Chlorchestra_Harpaster" : {
+			"rarity" : "CommonCard",
+			"name" : "Chlorchestra Harpaster",
+			"type" : "Plant",
+			"top_counter" : "1",
+			"bot_counter" : "2",
+			"cost" : 4,
+			"effect" : "Once per turn, if the environment is sunny, you can summon 1 Bulb Token (0/1 Plant, no additional effect).",
+		}, 
+		"Chlorchestra_Timpaniris" : {
+			"rarity" : "CommonCard",
+			"name" : "Chlorchestra Timpaniris",
+			"type" : "Plant",
+			"top_counter" : "1",
+			"bot_counter" : "2",
+			"cost" : 4,
+			"effect" : "Once per turn, if the environment is sunny, you can destroy one target creature.",
+		},
+		"Chlorchestra_Glorgan" : {
+			"rarity" : "CommonCard",
+			"name" : "Chlorchestra Glorgan",
+			"type" : "Plant",
+			"top_counter" : "1",
+			"bot_counter" : "2",
+			"cost" : 4,
+			"effect" : "Once per turn, if the environment is sunny, you can discard 1 card from your opponent’s hand.",
+		},
+		"Chlorchestra_Trumpansy" : {
+			"rarity" : "RareCard",
+			"name" : "Chlorchestra Trumpansy",
+			"type" : "Plant",
+			"top_counter" : "1",
+			"bot_counter" : "2",
+			"cost" : 6,
+			"effect" : "Once per turn, if the environment is sunny, you can look at the top 3 cards of your deck; add 1 creature from those 3 cards to your hand and send the rest to the graveyard. If none were creatures, send all 3 to the graveyard.",
+		},
+		"Chlorchestra_Conductare" : {
+			"rarity" : "RareCard",
+			"name" : "Chlorchestra Timpaniris",
+			"type" : "Earth",
+			"top_counter" : "2",
+			"bot_counter" : "3",
+			"cost" : 10,
+			"effect" : "When this card is summoned, add 1 “Clorchestra” creature from your deck to your hand.",
+		},
+		
 	},
 	"Spells" : {
 		"Counter" : {
+			"rarity" : "RareCard",
 			"name" : "Counter",
 			"cost" : 4,
 			"effect" : "Activate this spell in response to your opponent’s activation of a spell. Negate the activation of that spell."
 		},
 		"Umbot_Parade" : {
+			"rarity" : "SecretCard",
 			"name" : "Umbot Parade",
 			"cost" : 14,
 			"effect" : "(Artifact Equivalent) Destroy this card if there is no eclipse. All “Umbot” creatures gain (Cascade Equivalent).",
 		},
 		"Umbot_Factory" : {
+			"rarity" : "RareCard",
 			"name" : "Umbot Factory",
 			"cost" : 8,
 			"effect" : "Add an “Umbot” creature from your deck to your hand."
 		},
 		"Clear_Weather" : {
+			"rarity" : "LegendaryCard",
 			"name" : "Clear Weather",
 			"cost" : 5,
-			"effect" : "Set the Environment to Cloudy"
+			"effect" : "Set the Environment to Cloudy."
 		},
+		"Compost" : {
+			"rarity" : "CommonCard",
+			"name" : "Compost",
+			"cost" : 5,
+			"effect" : "Sacrifice 1 creature you control; target Plant creature gains its Attack and HP."
+		},
+		"Connected_Roots" : {
+			"rarity" : "LegendaryCard",
+			"name" : "Connected Roots",
+			"cost" : 15,
+			"effect" : "The HP of each Plant creature you control becomes equal to the total HP of all Plant creatures you control."
+		},
+		"Chlorchestra_Rehearsal" : {
+			"rarity" : "RareCard",
+			"name" : "Chlorchestra Rehearsal",
+			"cost" : 6,
+			"effect" : "You can only activate this card if you control a “Chlorchestra” creature. Draw 2 cards and discard 1 card."
+		},
+		"Chlorchestra_Symphony" : {
+			"rarity" : "SecretCard",
+			"name" : "Chlorchestra_Symphony",
+			"cost" : 20,
+			"effect" : "You can only activate this card if you control at least 1 “Chlorchestra Blossoon”, “Chlorchestra Harpaster”, “Chlorchestra Timpaniris”, “Chlorchestra Glorgan”, “Chlorchestra Trumpansy”, and “Chlorchestra Conductare”. Destroy all cards your opponent controls."
+		},
+		"Copy_Machine" : {
+			"rarity" : "RareCard",
+			"name" : "Copy Machine",
+			"cost" : 10,
+			"effect" : "Create a 0/1 token with the same name and effect as a target Robotic creature you control."
+		},
+		"Bird_Call" : {
+			"rarity" : "CommonCard",
+			"name" : "Bird_Calle",
+			"cost" : 10,
+			"effect" : "Add 1 “Weatherbird” or “Lunarbird” creature from your deck to your hand.",
+		},
+		
 	}
 	
 }
