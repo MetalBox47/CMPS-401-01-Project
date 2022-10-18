e1bd23f8f2de12bd39036ab809b57dc2b077d6bd
Marquinoli <mfigueroaferez@gmail.com>
2022-10-17 21:13:11 -0500

    Changing playspace background and adding some more assets

--- Cards/CardBase.gd
+++ Cards/CardBase.gd
@@ -7,12 +7,12 @@ onready var spells = CardDatabase.DATA.Spells
 onready var creatures_arr = CardDatabase.CREATURES
 
 # We define the variables that are gonna be necessary to extract the assets
-var CardType = 'Spells'
-var CardName = 'Umbot_Factory'
+var CardType = "Creatures"
+var CardName = "Umbot_Skal"
 
 # Here we filter which Card Background and which Card Image to use
-# Card Background = Frame (Depends on rarity)
-# Card Image = The small img inside the Frame (depends on card name)
+# Card Background => Frame (Depends on rarity)
+# Card Image => The small img inside the Frame (depends on card name)
 onready var CardInfo = CardDatabase.DATA[CardType][CardName]
 onready var CardImg = str("res://Assets/",CardInfo.name, ".png")
 onready var CardBackgroundImg = str("res://Asset/",CardInfo.rarity,".png")
@@ -20,11 +20,9 @@ onready var CardBackgroundImg = str("res://Asset/",CardInfo.rarity,".png")
 var CardTexture
 var CardImgContainer
 var CardSize = rect_size
+# rect_size is the size of the "CardBase" container
+# We may not use CardSize, this is just to show how to extract it.
 func _ready():
-	
-	  # rect_size is the size of the "CardBase" container
-	# We may not use CardSize, this is just to show how to extract it.
-	
 	# loading the small CardImg and setting its scale to the card borders
 	CardImgContainer = get_node("HBoxContainer/Elements/CardImgContainer")
 	CardTexture = get_node("HBoxContainer/Elements/CardImgContainer/Card")
@@ -44,12 +42,30 @@ func _ready():
 			var Health = CardInfo["bot_counter"]
 			$HBoxContainer/Elements/HBoxContainer/Health/Label.text = str(Health)
 			$HBoxContainer/Elements/HBoxContainer/Attack/Label.text = str(Attack)
-		
 	
-	
+	# Re-scales the invisible button to be hovered
+	$Focus.rect_scale *= CardSize/$Focus.rect_size
 	print($Background.scale)
 	print(CardTexture.texture.get_size())
+	
+# List of scales to be used for the card onhover resizing
+var state = "OutHand"
+onready var new_scale = rect_scale * 1.1
+onready var original_scale = rect_scale
+
+func _physics_process(delta):
+	match state:
+		"InHand":
+			rect_scale = new_scale
+		"OutHand":
+			rect_scale = original_scale
 
 
-
+# functions that emit signals to resize the card when mousehovered
+func _on_Focus_mouse_entered():
+	state = "InHand"
+	print("Entered")
+func _on_Focus_mouse_exited():
+	state = "OutHand"
+	print("Exited")
 
--- PlaySpace/Playspace.gd
+++ PlaySpace/Playspace.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
 const CardBase = preload("res://Cards/CardBase.tscn") #preloading the CardBase
+const CardFocus = preload("res://Cards/CardBase.gd")
 const CardSize = Vector2(125,175) #This is half the size of the card
 
 # The PlayerDeck has to be a 2D array, [CardName, CardType]
 # This is a test dummy player deck
-const PlayerDeck = [["Umbot_Factory", "Spells"],["Umbot_Factory", "Spells"], ["Umbot_Factory", "Spells"]]
+const PlayerDeck = [["Umbot_Factory", "Spells"],["Umbot_Lanu", "Creatures"], ["Umbot_Plum", "Creatures"], ["Umbot_Zuma", "Creatures"], ["Umbot_Skal", "Creatures"]]
 
 var CardSelected = [] # Holds an int
 var DeckSize = PlayerDeck.size()
 
 
-var windowWidth = ProjectSettings.get_setting("display/window/size/width")
-var windowHeight = ProjectSettings.get_setting("display/window/size/height")
-var window = Vector2(windowWidth, windowHeight)
+#var windowWidth = ProjectSettings.get_setting("display/window/size/width")
+#var windowHeight = ProjectSettings.get_setting("display/window/size/height")
+#var window = Vector2(windowWidth, windowHeight)
  
 
 
@@ -21,13 +22,10 @@ func _ready():
 	# Setting the scale of the background to fit the window
 	var viewportWidth = get_viewport().size.x
 	var viewportscale = viewportWidth/$DuelBackground.texture.get_size().x
+	var vboxcontainerscale = viewportWidth/$VBoxContainer.rect_size.x
 	$DuelBackground.set_scale(Vector2(viewportscale, viewportscale))
-	$VBoxContainer.set_scale(Vector2(viewportscale, viewportscale))
+	$VBoxContainer.set_scale(Vector2(vboxcontainerscale, vboxcontainerscale))
 	
-	
-	
-	pass
-
 
 var incrementer = 0
 # Draws a card from the deck
@@ -38,17 +36,17 @@ func drawCard():
 	new_card.CardType = PlayerDeck[CardSelected][1]
 	new_card.CardName = PlayerDeck[CardSelected][0]
 	
-	
-	
 	var PlayerDeckNode = get_node("VBoxContainer/Player1Side/VBoxContainer/HBoxContainer/VBoxContainer/PlayerDeck")
 	var CardPos_x = PlayerDeckNode.rect_global_position.x
 	var CardPos_y = PlayerDeckNode.rect_global_position.y
 	new_card.rect_position = Vector2(CardPos_x+incrementer, CardPos_y) # Assigns a global position to the card with respect to its container
 	new_card.rect_scale *= CardSize/new_card.rect_size # Re-scaling to fit the playspace
+	
 	$Cards.add_child(new_card)
+	
 	PlayerDeck.erase(PlayerDeck[CardSelected])
 	DeckSize -= 1
 	incrementer += 100
 	
-
+	
 	
