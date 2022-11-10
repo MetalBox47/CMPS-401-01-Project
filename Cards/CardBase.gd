2c38439abc3c6691a683dd3b58b929a01627b1cf
Mae Delamar <69863280+PrehistoricDolphin@users.noreply.github.com>
2022-11-10 10:25:37 -0600

    Added Changes Committed to Wrong Branch

--- Cards/CardBase.gd
+++ Cards/CardBase.gd
@@ -16,7 +16,7 @@ var CardName = "Umbot_Skal"
 # Card Background => Frame (Depends on rarity)
 # Card Image => The small img inside the Frame (depends on card name)
 onready var CardInfo = CardDatabase.DATA[CardType][CardName]
-onready var CardImg = str("res://Assets/",CardInfo.name, ".png")
+onready var directory = Directory.new()
 onready var CardBackgroundImg = str("res://Asset/",CardInfo.rarity,".png")
 onready var cost = int(CardInfo.cost)
 onready var base_atk
@@ -34,8 +34,10 @@ func _ready():
 	# loading the small CardImg and setting its scale to the card borders
 	CardImgContainer = get_node("HBoxContainer/Elements/CardImgContainer")
 	CardTexture = get_node("HBoxContainer/Elements/CardImgContainer/Card")
-	$HBoxContainer/Elements/CardImgContainer/Card.texture = load(CardImg)
-	$HBoxContainer/Elements/CardImgContainer/Card.scale *= CardImgContainer.get_size()/CardTexture.texture.get_size()
+	if directory.file_exists(str("res://Assets/",CardInfo.name, ".png")):
+		var CardImg = str("res://Assets/",CardInfo.name, ".png")
+		$HBoxContainer/Elements/CardImgContainer/Card.texture = load(CardImg)
+		$HBoxContainer/Elements/CardImgContainer/Card.scale *= CardImgContainer.get_size()/CardTexture.texture.get_size()
 #	$Focus.rect_scale *= CardSize/$Focus.rect_size
 	# Loading the background texture
 	$Background.texture = load(CardBackgroundImg)
@@ -60,8 +62,6 @@ func _ready():
 	
 	# Re-scales the invisible button to be hovered
 	$Focus.rect_scale *= CardSize/$Focus.rect_size
-	print($Background.scale)
-	print(CardTexture.texture.get_size())
 
 # List of scales to be used for the card onhover resizing
 var state = "OutHand"
